327b72dc009ca57fc39d6982ba72649045649cf1
T0NG.K <39180519+t0ngk@users.noreply.github.com>
2024-03-09 05:48:54 +0700

    🐛 : Invalid query

--- src/routes/admin/dashboard.ts
+++ src/routes/admin/dashboard.ts
@@ -5,40 +5,36 @@ import { AdminLoginRequest } from "../../libs/types/AdminLoginRequest";
 
 const router = Router();
 
-router.get(
-  "/",
-  isAdminLogin,
-  async (_: AdminLoginRequest, res: Response) => {
-    const totalCamps = await prisma.camp.count({
-      where: {
-        name: {
-          not: "Staff",
-        },
+router.get("/", isAdminLogin, async (_: AdminLoginRequest, res: Response) => {
+  const totalCamps = await prisma.camp.count({
+    where: {
+      NOT: {
+        name: "Staff",
       },
-    });
-    const totalCampers = await prisma.camper.count({
-      where: {
-        Camp: {
-          name: {
-            not: "Staff",
-          },
-        },
-      },
-    });
-    const totalStaffs = await prisma.camper.count({
-      where: {
+    },
+  });
+  const totalCampers = await prisma.camper.count({
+    where: {
+      NOT: {
         Camp: {
           name: "Staff",
         },
       },
-    });
+    },
+  });
+  const totalStaffs = await prisma.camper.count({
+    where: {
+      Camp: {
+        name: "Staff",
+      },
+    },
+  });
 
-    res.send({
-      totalCamps,
-      totalCampers,
-      totalStaffs,
-    });
-  }
-);
+  res.send({
+    totalCamps,
+    totalCampers,
+    totalStaffs,
+  });
+});
 
 export default router;
