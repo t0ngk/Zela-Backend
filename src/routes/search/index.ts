d007e66e424d84c25ec904b22272aedb9019bb42
T0NG.K <39180519+t0ngk@users.noreply.github.com>
2024-03-10 00:20:18 +0700

    🔧 : Not include user camper

--- src/routes/search/index.ts
+++ src/routes/search/index.ts
@@ -1,5 +1,7 @@
 import { Router, Request, Response } from "express";
 import prisma from "../../libs/prisma";
+import isCamperLogin from "../../libs/middlewares/isCamperLogin";
+import { CamperLoginRequest } from "../../libs/types/CamperLoginRequest";
 
 const router = Router();
 
@@ -14,73 +16,85 @@ router.get("/camps", async (_, res: Response) => {
   res.send(camps);
 });
 
-router.get("/campers/", async (req: Request, res: Response) => {
-  try {
-    const name = req.query.name as string;
-    const camp = req.query.camp as string;
-    if (!name && !camp) {
-      const campers = await prisma.camper.findMany(
-        {select: {
-          id: true,
-          name: true,
-          Camp: true,
-          ProfileImage: {
-            select: {
-              url: true,
-            }
-          }
-        }}
-      );
-      return res.send(campers);
-    }
-    if (!camp) {
-      const campers = await prisma.camper.findMany({
-        where: {
-          name: {
-            contains: name,
+router.get(
+  "/campers/",
+  isCamperLogin,
+  async (req: CamperLoginRequest, res: Response) => {
+    try {
+      const name = req.query.name as string;
+      const camp = req.query.camp as string;
+      if (!name && !camp) {
+        const campers = await prisma.camper.findMany({
+          where: {
+            NOT: {
+              name: req.user?.name,
+            },
           },
-        },
-        select: {
-          id: true,
-          name: true,
-          Camp: true,
-          ProfileImage: {
-            select: {
-              url: true,
+          select: {
+            id: true,
+            name: true,
+            Camp: true,
+            ProfileImage: {
+              select: {
+                url: true,
+              },
+            },
+          },
+        });
+        return res.send(campers);
+      }
+      if (!camp) {
+        const campers = await prisma.camper.findMany({
+          where: {
+            name: {
+              contains: name,
+            },
+            NOT: {
+              name: req.user?.name,
             }
-          }
-        },
+          },
+          select: {
+            id: true,
+            name: true,
+            Camp: true,
+            ProfileImage: {
+              select: {
+                url: true,
+              },
+            },
+          },
+        });
+        return res.send(campers);
+      } else {
+        const campers = await prisma.camper.findMany({
+          where: {
+            name: {
+              contains: name,
+            },
+            Camp: {
+              name: camp,
+            },
+          },
+          select: {
+            id: true,
+            name: true,
+            Camp: true,
+            ProfileImage: {
+              select: {
+                url: true,
+              },
+            },
+          },
+        });
+        return res.send(campers);
+      }
+    } catch (error) {
+      console.log(error);
+      return res.status(500).send({
+        message: "Internal Server Error",
       });
-      return res.send(campers);
-    } else {
-      const campers = await prisma.camper.findMany({
-        where: {
-          name: {
-            contains: name,
-          },
-          Camp: {
-            name: camp,
-          },
-        },
-        select: {
-          id: true,
-          name: true,
-          Camp: true,
-          ProfileImage: {
-            select: {
-              url: true,
-            }
-          }
-        },
-      });
-      return res.send(campers);
     }
-  } catch (error) {
-    console.log(error);
-    return res.status(500).send({
-      message: "Internal Server Error",
-    });
   }
-});
+);
 
 export default router;
